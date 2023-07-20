5c6fdf2d5b58e389443e58d3b55b449c591efb3d
Frank Gimeno <frankgimeno3@gmail.com>
2023-07-20 05:23:49 +0200

    mejoro barra produccion

--- components/navherramientas/navproduccion.tsx
+++ components/navherramientas/navproduccion.tsx
@@ -13,7 +13,7 @@ export default function Navproduccion() {
       </div>
       <hr className="mt-5"/>
       <div className=" flex flex-col">
-        <h2 className="text-white pl-4 mt-5 text-sm">Opciones para <span className="font-bold">Fichas de clientes</span></h2>
+        <h2 className="text-white pl-4 mt-5 text-sm">Opciones para <span className="font-bold">Producción</span></h2>
         <div className="flex flex-row bg-white bg-opacity-20 my-5">
           <div className="bg-white px-1 "></div>
           <button className="bg-white bg-opacity-25 text-sm w-full py-1 text-white text-left pl-5 hover:font-bold">
